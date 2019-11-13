ca7bd00fcc225f771eed4061c18db22c6a787afa
Shubham Jindal <shubhamjindal1234@gmail.com>
2019-11-13 14:32:29 +1100

    minor changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -6,6 +6,7 @@
 
 var mongo = {
     URI: process.env.MONGO_URI || 'mongodb://localhost/doc-profiler-backend',
+    // URI: process.env.MONGO_URI || 'mongodb://mongo:27017/doc-profiler-backend', //for docker
     //URI: process.env.MONGO_URI || "mongodb://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@localhost/"+process.env.MONGO_DBNAME_DEV,
     port: 27017
 };
