93f4270a285b8e5f2d779caeee4a2680d5f9a127
Navit Choudhary <n.choudhary@deakin.edu.au>
2019-11-22 10:24:14 +1100

    changes for test branch

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,9 +5,9 @@
 'use strict';
 
 var mongo = {
-    URI: process.env.MONGO_URI || 'mongodb://localhost/doc-profiler-backend',
+    //URI: process.env.MONGO_URI || 'mongodb://localhost/doc-profiler-backend',
     // URI: process.env.MONGO_URI || 'mongodb://mongo:27017/doc-profiler-backend', //for docker
-    //URI: process.env.MONGO_URI || "mongodb://"+process.env.MONGO_USER_DOC_PROFILER+":"+process.env.MONGO_PASS_DOC_PROFILER+"@localhost/"+process.env.MONGO_DBNAME_DOC_PROFILER_TEST,
+    URI: process.env.MONGO_URI || "mongodb://"+process.env.MONGO_USER_DOC_PROFILER+":"+process.env.MONGO_PASS_DOC_PROFILER+"@localhost/"+process.env.MONGO_DBNAME_DOC_PROFILER_TEST,
     port: 27017
 };
 
